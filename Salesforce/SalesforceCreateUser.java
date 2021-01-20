354cfe1e5878de038ae69275aae7d65a9345c1d4
Renuvenkat77 <64781936+Renuvenkat77@users.noreply.github.com>
2021-01-19 18:43:43 -0600

    Update SalesforceCreateUser.java

--- Salesforce/SalesforceCreateUser.java
+++ Salesforce/SalesforceCreateUser.java
@@ -18,39 +18,33 @@ public class SalesforceCreateUser {
 		pwd.sendKeys("Renuka123!");
 		WebElement login1= driver.findElement(By.id("Login"));
 		login1.click();	
-		
 		Thread.sleep(40000);
 		String win= driver.getWindowHandle();
 		WebElement createmenu= driver.findElement(By.xpath("//span[@class='label']"));
-        createmenu.click();
+                createmenu.click();
 		WebElement useroption= driver.findElement(By.xpath("//span[@class='slds-align-middle'][1]"));
-        useroption.click();
-       Thread.sleep(40000);
+                useroption.click();
+                Thread.sleep(40000);
        
-        for(int i=8; i<=10; i++)
+        for(int i=1; i<=5; i++)
 		{
        WebElement frame1= driver.findElement(By.xpath("//iframe[@title='New User ~ Salesforce - Developer Edition']"));
        driver.switchTo().frame(frame1);
        String newuserwin=driver.getWindowHandle();
-        	//WebElement ufirstname= driver.findElement(By.xpath("//label[@for='name_firstName']"));
-       driver.findElement(By.id("name_firstName")).sendKeys("Shravan"+i);
-        //WebElement ufirstname= driver.findElement(By.xpath("//*[@id=\'name_firstName\']"));
-       driver.findElement(By.id("name_lastName")).sendKeys("venkat"+i); 
-        // WebElement ulastname= driver.findElement(By.xpath("//input[@id='name_lastName']"));
-        //WebElement ulastname= driver.findElement(By.xpath("//*[@id=\'name_lastName\']"));
-       driver.findElement(By.id("Alias")).sendKeys("Shra"+i);
+        driver.findElement(By.id("name_firstName")).sendKeys("Shravan"+i);
+        driver.findElement(By.id("name_lastName")).sendKeys("venkat"+i); 
+        driver.findElement(By.id("Alias")).sendKeys("Shra"+i);
        driver.findElement(By.id("Email")).sendKeys("Shravan"+i+"@gmail.com"); 
        driver.findElement(By.id("Username")).sendKeys("Shravanv"+i+"@gmail.com");
-       
-        /*WebElement profile= driver.findElement(By.xpath("//select[@id='Profile']"));
+       WebElement profile= driver.findElement(By.xpath("//select[@id='Profile']"));
         profile.click();
         Select s1= new Select(profile);
         s1.selectByIndex(0);*/
         WebElement savennew= driver.findElement(By.xpath("//td[@class='pbButton']/*[2]"));
-	    savennew.click();
-	    Thread.sleep(7000);
-driver.findElement(By.xpath("//button[@class='slds-button slds-button--neutral uiButton--neutral uiButton forceActionButton']//*[1]")).click();
-	driver.switchTo().window(newuserwin);
+	  savennew.click();
+	  Thread.sleep(7000);
+         driver.findElement(By.xpath("//button[@class='slds-button slds-button--neutral uiButton--neutral uiButton forceActionButton']//*[1]")).click();
+	 driver.switchTo().window(newuserwin);
 	
 	
 		}
